bfc7f1d85ce9d4495356f87311188f4dab6f5e52
Isaiah Hoffer <hoffer.isaiah11@gmail.com>
2025-07-27 15:41:25 -0600

    CS 2080

---
+++ CS/CS 2080/Assignments/Assignment6/math_utils.h
@@ -0,0 +1,13 @@
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+// Function declarations for math operations
+int add(int a, int b);
+
+int subtract(int a, int b);
+
+int multiply(int a, int b);
+
+int divide(int a, int b);
+
+#endif // MATH_UTILS_H
---
+++ CS/CS 2080/Labs/HofferIsaiahLab10/lab10/utils.c
@@ -0,0 +1,11 @@
+#include "utils.h"
+
+//Add Two Numbers 
+int add(int a, int b) {
+	return a + b; 
+} //add Function 
+
+//Multipy Two Numbers 
+int multiply(int a,int b) { 
+	return a * b; 
+} //multiply Function 
